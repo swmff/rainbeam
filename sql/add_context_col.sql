88c35a9b1ee4591c71c5ee25fb23940a748ee0f5
trisua <tri@swmff.org>
2024-11-13 17:11:04 -0500

    chore: organize sql and job

--- sql/add_context_col.sql
+++ sql/add_context_col.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "xquestions" ADD COLUMN "context" TEXT DEFAULT '{}';
 ALTER TABLE "xprofiles" ADD COLUMN "token_context" TEXT DEFAULT '[]';
+ALTER TABLE "xcomments" ADD COLUMN "context" TEXT DEFAULT '{}';
