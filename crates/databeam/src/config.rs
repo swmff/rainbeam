7c1ba1b014dc99ecc08dde6903864a349f1b13d8
trisua <tri@swmff.org>
2024-12-01 11:50:57 -0500

    fix: profile metadata loading add(databeam): move config location

--- crates/databeam/src/config.rs
+++ crates/databeam/src/config.rs
@@ -1,7 +1,7 @@
 //! Application config manager
 use serde::{Deserialize, Serialize};
 use shared::fs;
-use std::{env, io::Result};
+use std::io::Result;
 
 /// Configuration file
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -25,22 +25,10 @@ impl Config {
 
     /// Pull configuration file
     pub fn get_config() -> Self {
-        let home = env::var("HOME").expect("failed to read $HOME");
+        let c = fs::canonicalize(".").unwrap();
+        let here = c.to_str().unwrap();
 
-        if let Err(_) = fs::read_dir(format!("{home}/.config/xsu-apps/dataman")) {
-            // make sure .config exists
-            fs::mkdir(format!("{home}/.config")).expect("failed to create .config directory");
-
-            // make sure .config/xsu-apps exists
-            fs::mkdir(format!("{home}/.config/xsu-apps"))
-                .expect("failed to create xsu-apps directory");
-
-            // create .config/xsu-apps/dataman
-            fs::mkdir(format!("{home}/.config/xsu-apps/dataman"))
-                .expect("failed to create dataman directory")
-        }
-
-        match fs::read(format!("{home}/.config/xsu-apps/dataman/config.toml")) {
+        match fs::read(format!("{here}/.config/databeam/config.toml")) {
             Ok(c) => Config::read(c),
             Err(_) => {
                 Self::update_config(Self::default()).expect("failed to write default config");
@@ -51,10 +39,11 @@ impl Config {
 
     /// Update configuration file
     pub fn update_config(contents: Self) -> Result<()> {
-        let home = env::var("HOME").expect("failed to read $HOME");
+        let c = fs::canonicalize(".").unwrap();
+        let here = c.to_str().unwrap();
 
         fs::write(
-            format!("{home}/.config/xsu-apps/dataman/config.toml"),
+            format!("{here}/.config/databeam/config.toml"),
             toml::to_string_pretty::<Self>(&contents).unwrap(),
         )
     }
