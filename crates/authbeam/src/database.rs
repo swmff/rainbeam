9d6d9b05ac4cb2c4a85512f2045be00809fe073a
trisua <tri@swmff.org>
2024-12-17 19:03:52 -0500

    add(ui): bigger notification text

--- crates/authbeam/src/database.rs
+++ crates/authbeam/src/database.rs
@@ -1480,18 +1480,6 @@ impl Database {
                     return Err(DatabaseError::Other);
                 };
 
-                // sites by user
-                let query: &str =
-                    if (self.base.db.r#type == "sqlite") | (self.base.db.r#type == "mysql") {
-                        "DELETE FROM \"xposts\" WHERE \"owner\" = ?"
-                    } else {
-                        "DELETE FROM \"xposts\" WHERE \"owner\" = $1"
-                    };
-
-                if let Err(_) = sqlquery(query).bind::<&String>(&id).execute(c).await {
-                    return Err(DatabaseError::Other);
-                };
-
                 // ...
                 self.base
                     .cachedb
